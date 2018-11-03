78f6420dafcdfcfa770d22319135902ba42baca7
Marcus Lugg <marcuslugg@googlemail.com>
2018-11-03 11:32:27 +0000

    ENH: Add perceptron class

---
+++ perceptron.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+
+class Perceptron:
+    """Perceptron classifier.
+
+    :param float eta: learning rate (between 0 and 1)
+    :param int n_iter: training iterations
+    :param int random_state: random number generator seed for random weight initialisation
+    :param np.ndarray weights: weights after fitting
+    :param list errors: number of mis-classifications (updates) in each epoch
+    """
+
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.weights = np.ndarray([])
+        self.errors = []
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """Fit training data.
+        :param np.ndarray X: training vectors in a matrix of size n_samples by n_features
+        :param np.ndarray y: target values in a vector
+
+        :return Perceptron:
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.weights = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.errors = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.weights[1:] += update * xi
+                self.weights[0] += update
+                errors += int(update != 0)
+            self.errors.append(errors)
+
+        return self
+
+    def net_input(self, X):
+        """Calculate net input.
+
+        :param np.ndarray X: input values to net
+
+        :return np.ndarray: net input
+        """
+        net_input = np.dot(X, self.weights[1:]) + self.weights[0]
+        return net_input
+
+    def predict(self, X):
+        """Predict class label after unit step.
+
+        :param np.ndarray X: input values to predict on
+        """
+        predictions = np.where(self.net_input(X) >= 0, 1, -1)
+        return predictions
