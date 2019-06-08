f86fc5f62d1e9d4835f1aadafb3f9c5dffd6e1ac
Marcus Lugg <marcuslugg@googlemail.com>
2019-06-08 16:01:43 +0100

    CHO: Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='machine-learning',
+    packages=find_packages(),
+    url='http://www.github.com/cortadocodes/machine-learning',
+    author='Marcus Lugg',
+    author_email='marcuslugg@googlemail.com',
+    install_requires=[
+        'matplotlib>=3.1',
+        'numpy>=1.16',
+        'pandas>=0.24'
+    ],
+    extras_require = {
+        'development': [
+            'flake8',
+            'pydocstyle',
+            'pytest'
+        ]
+    }
+)
