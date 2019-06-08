e8663a5304fd26f547856988d816e3d53ffc1a9e
Marcus Lugg <marcuslugg@googlemail.com>
2019-06-08 15:50:34 +0100

    DOC: Add some implementation notes

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -15,9 +15,10 @@ class Perceptron:
     """
 
     def __init__(self, learning_rate=0.01, number_of_training_iterations=50, random_state_seed=1):
-        """Initialise a Perceptron.
+        """Initialise a Perceptron. Note that for the learning rate to have an effect on the classification outcome, the
+        weights must be initialised to non-zero values.
 
-        :param float learning rate: should be between 0 and 1
+        :param float learning_rate: should be between 0 and 1
         :param int number_of_training_iterations:
         :param int random_state_seed: for random weight initialisation
         :var np.array weights: internal weights of the Perceptron
@@ -26,8 +27,8 @@ class Perceptron:
         self.learning_rate = learning_rate
         self.number_of_training_iterations = number_of_training_iterations
         self.random_state = random_state_seed
-
         self.weights = np.array([])
+        # Collect the number of errors per epoch in order to analyse the model during training.
         self.errors_per_epoch = []
 
     def fit(self, training_samples, true_labels):
